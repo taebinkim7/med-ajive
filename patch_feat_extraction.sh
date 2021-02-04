5a749dec08d178a640a0c4f248c64364d5d68173
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-02-04 18:14:45 -0500

    Update patch_feat_extraction.sh

--- patch_feat_extraction.sh
+++ patch_feat_extraction.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -p gpu
-#SBATCH -t 04-00:00:00
-#SBATCH --qos gpu_access
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=9G
+#SBATCH --time=4:00:00
+#SBATCH --partition=gpu
+#SBATCH --output=run-%j.log
 #SBATCH --gres=gpu:1
+#SBATCH --qos=gpu_access
+#SBATCH --mail-type=end
+#SBATCH --mail-user=taebinkim@unc.edu
+
+source ~/miniconda3/etc/profile.d/conda.sh
+
+conda activate med-ajive
 
 python scripts/patch_feat_extraction.py
