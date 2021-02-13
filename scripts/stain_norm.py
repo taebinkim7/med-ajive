20837fa671fedaf576a866acee7ad51e4ece3060
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-02-13 16:28:12 -0500

    Update stain_norm.py

--- scripts/stain_norm.py
+++ scripts/stain_norm.py
@@ -8,35 +8,35 @@ from cbcs_joint.Paths import Paths
 
 def get_stain(img, Io, alpha, beta):  
     # reshape image
-    img = img.reshape((-1,3))
+    img = img.reshape((-1, 3))
 
     # calculate optical density
-    OD = -np.log10((img.astype(np.float)+1)/Io)
+    OD = -np.log10((img.astype(np.float) + 1) / Io)
     
     # remove transparent pixels
-    ODhat = OD[~np.any(OD<beta, axis=1)]
+    ODhat = OD[~np.any(OD < beta, axis=1)]
         
     # compute eigenvectors
     eigvals, eigvecs = np.linalg.eigh(np.cov(ODhat.T))
     
     # project on the plane spanned by the eigenvectors corresponding to the two 
     # largest eigenvalues    
-    That = ODhat.dot(eigvecs[:,1:3])
+    That = ODhat.dot(eigvecs[:, 1:3])
     
-    phi = np.arctan2(That[:,1],That[:,0])
+    phi = np.arctan2(That[:, 1],That[:, 0])
     
     minPhi = np.percentile(phi, alpha)
-    maxPhi = np.percentile(phi, 100-alpha)
+    maxPhi = np.percentile(phi, 100 - alpha)
     
-    vMin = eigvecs[:,1:3].dot(np.array([(np.cos(minPhi), np.sin(minPhi))]).T)
-    vMax = eigvecs[:,1:3].dot(np.array([(np.cos(maxPhi), np.sin(maxPhi))]).T)
+    vMin = eigvecs[:, 1:3].dot(np.array([(np.cos(minPhi), np.sin(minPhi))]).T)
+    vMax = eigvecs[:, 1:3].dot(np.array([(np.cos(maxPhi), np.sin(maxPhi))]).T)
     
     # a heuristic to make one vector corresponding to the first color and the 
     # other corresponding to the second
     if vMin[0] > vMax[0]:
-        stain = np.array((vMin[:,0], vMax[:,0])).T
+        stain = np.array((vMin[:, 0], vMax[:, 0])).T
     else:
-        stain = np.array((vMax[:,0], vMin[:,0])).T
+        stain = np.array((vMax[:, 0], vMin[:, 0])).T
     
     # rows correspond to channels (RGB), columns to OD values
     Y = np.reshape(OD, (-1, 3)).T
@@ -45,7 +45,7 @@ def get_stain(img, Io, alpha, beta):
     C = np.linalg.lstsq(stain,Y, rcond=None)[0]
     
     # normalize stain concentrations
-    maxC = np.array([np.percentile(C[0,:], 99), np.percentile(C[1,:],99)])
+    maxC = np.array([np.percentile(C[0, :], 99), np.percentile(C[1, :], 99)])
     
     return stain, C, maxC
     
@@ -57,17 +57,17 @@ def stain_norm(img, stainRef, maxCRef, Io, alpha, beta, saveFile=None):
     stain, C, maxC = get_stain(img, Io, alpha, beta)
         
     # normalize stain concentrations
-    maxC = np.array([np.percentile(C[0,:], 99), np.percentile(C[1,:],99)])
+    maxC = np.array([np.percentile(C[0, :], 99), np.percentile(C[1, :], 99)])
     tmp = np.divide(maxC,maxCRef)
     C2 = np.divide(C,tmp[:, np.newaxis])
     
     # recreate the image using reference mixing matrix
     Inorm = np.multiply(Io, 10**(-stainRef.dot(C2)))
-    Inorm[Inorm>255] = 254
+    Inorm[Inorm > 255] = 254
     Inorm = np.reshape(Inorm.T, (h, w, 3)).astype(np.uint8)  
     
     if saveFile is not None:
-        Image.fromarray(Inorm).save(saveFile+'.png')
+        Image.fromarray(Inorm).save(saveFile + '.png')
 
     return
 
