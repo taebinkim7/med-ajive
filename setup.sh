fcb8f19176a5363f6ce92b58507e5c812ac00753
Taebin Kim <44902482+taebinkim7@users.noreply.github.com>
2021-02-21 23:35:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ pip install .
 pip install scikit-image==0.14.1
 git clone https://github.com/idc9/explore.git
 python explore/setup.py install
+pip install scikit-image==0.14.1
 pip install jive
 pip install https://download.pytorch.org/whl/cu100/torch-1.0.0-cp37-cp37m-linux_x86_64.whl
 pip install torchvision==0.2.1
